88414a3f9094a0ef0209dba89fdb72d67c60f06f
Shelpidy <ingshelpidy.com>
2023-11-22 16:47:37 +0000

    Modify the cd yml file

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
-FROM node:18
-WORKDIR /app
+FROM node:alpine
+WORKDIR /usr/app
 COPY package*.json ./
 RUN npm install
 COPY . .
--- fimiz.sh
+++ fimiz.sh
@@ -1,19 +1,18 @@
-#!/bin/bash
 echo "Setting up envs"
 
 export PORT=6000
 export MAIL_PORT=465
-export MAIL_HOST="smtp.mail.yahoo.com"
-export MAIL_USER="mexu.sl@yahoo.com"
-export MAIL_PASS="rdooytekacavmmnn"
-export PG_DB_PASSWORD="Mexu2023"
-export PG_DB_USERNAME="fimiz"
-export PG_DB_HOST="fimiz-blog-db.ccn2o7dzia1z.us-east-1.rds.amazonaws.com"
-export PG_DB_NAME="fimiz-blog-db"
-export API_ACCESS_KEY="mexu2023medishk2"
-export SMS_API_KEY="9d035dbc"
-export SMS_SECRET_KEY="fUr53lsNeWpUL1iV"
-export ENV="production"
+export MAIL_HOST=smtp.mail.yahoo.com
+export MAIL_USER=mexu.sl@yahoo.com
+export MAIL_PASS=rdooytekacavmmnn
+export PG_DB_PASSWORD=Mexu2023
+export PG_DB_USERNAME=fimiz
+export PG_DB_HOST=fimiz-blog-db.ccn2o7dzia1z.us-east-1.rds.amazonaws.com
+export PG_DB_NAME=fimiz-blog-db
+# export API_ACCESS_KEY=mexu2023medishk2
+# export SMS_API_KEY=9d035dbc
+# export SMS_SECRET_KEY=fUr53lsNeWpUL1iV
+export ENV=production
 
 echo "Done setting up envs"
 
